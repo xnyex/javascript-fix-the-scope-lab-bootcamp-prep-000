9185f32540e66e68c91d184d385803cb78ae193f
Nye Smith <nyedess@gmail.com>
2018-05-10 19:38:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+  const two=2
+   n + two
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
